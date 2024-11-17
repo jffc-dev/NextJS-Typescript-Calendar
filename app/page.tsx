59d50e96023e7b11eeb3871102bfd7bd478b26f9
Javier Flores <jffc.dev@gmail.com>
2024-11-17 15:53:51 -0500

    feat: cta, features and testimonial added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { redirect } from "next/navigation";
 import Navbar from "./components/Navbar";
 import { auth } from "./lib/auth";
+import { Hero } from "./components/Hero";
+import { Logos } from "./components/Logos";
+import { Features } from "./components/Features";
+import { Testimonial } from "./components/Testimonial";
+import { Cta } from "./components/Cta";
 
 export default async function Home() {
   const session = await auth()
@@ -11,6 +16,11 @@ export default async function Home() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Navbar/>
+      <Hero/>
+      <Logos/>
+      <Features/>
+      <Testimonial/>
+      <Cta/>
     </div>
   );
 }
